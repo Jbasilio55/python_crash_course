af7ed4154150fc373383aee822ded7faeff90872
Jbasilio55 <j.l.basilio55@gmail.com>
2025-03-18 18:08:29 -0400

    math functions

---
+++ mathFunctions/mathFunctions.py
@@ -0,0 +1,15 @@
+import math #import math library 
+
+pi = 3.14
+x = 1
+y = 2
+z = 3
+
+print(round(pi)) # rounds number up or down to a whole number
+print(math.ceil(pi)) # rounds the decimal up to next whole number
+print(math.floor(pi)) # rounds the decimal down to next whole number
+print(abs(pi)) # this function will tell you how far a number is away from 0
+print(pow(pi, 2)) # this function will raise the base number to the number given (power) 3.14^2
+print(math.sqrt(9)) # this function will give you the square root of a number
+print(max(x,y,z)) #max find the max number out of the numbers given
+print(min(x,y,z)) #max find the min number out of the numbers given
