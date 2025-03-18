78cb55a31887c403bf943ffc663e5501bbde7f73
Jbasilio55 <j.l.basilio55@gmail.com>
2025-03-18 16:14:31 -0400

    multiple assignments

---
+++ multiple_assignments/main.py
@@ -0,0 +1,26 @@
+#multiple assignment = allows us to assign multiple variables  at the same time in one line of code
+
+# assigning each variable separately 
+name = "Bro"
+age = 22
+is_attractive = True
+
+print(name)
+print(age)
+print(is_attractive)
+
+# same assignment as above but this will; be done in one line
+
+name2, age2, is_attractive2 = "jorge", 35, True
+
+print(name2)
+print(age2)
+print(is_attractive2)
+
+# Another way to assign multiple variables, in this instance we are assigning the same value to 4 different var
+Spongebob = Patrick = Sandy = Squidward = 30
+
+print(Spongebob)
+print(Sandy)
+print(Patrick)
+print(Squidward)
